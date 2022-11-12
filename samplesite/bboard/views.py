0fcfc55bbca0c8c872bc7d19d4b5d172f0da0d25
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2022-11-12 14:17:54 +0500

    Models, Html

---
+++ samplesite/bboard/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Bb, Rubric
+
+
+def index(request):
+    rubrics = Rubric.objects.all()
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all() 
+    current_rubric = Rubric.objects.get(pk=rubric_id) 
+    context = {'bbs': bbs, 'rubrics': rubrics,'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
